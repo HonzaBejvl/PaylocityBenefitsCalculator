ce0969ba9dcbb78be46dcd9bc489e0c480dc3ba9
Jan Bejvl <janbejvl@live.com>
2024-02-11 23:51:01 +0300

    seed data

---
+++ Api.Model/Migrations/20240211134412_InitDbStructure.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDbStructure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dependents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Relationship = table.Column<int>(type: "int", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dependents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Dependents_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dependents_EmployeeId",
+                table: "Dependents",
+                column: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dependents");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Api.Model/Migrations/20240211134433_SeedEmployeeData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedEmployeeData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "FirstName", "LastName", "Salary", "DateOfBirth" },
+                values: new object[,]
+                {
+                    { 1, "LeBron", "James", 75420.99m, new DateTime(1984, 12, 30) },
+                    { 2, "Ja", "Morant", 92365.22m, new DateTime(1999, 8, 10) },
+                    { 3, "Michael", "Jordan", 143211.12m, new DateTime(1963, 2, 17) }
+                });
+
+            // Seed Dependent Data
+            migrationBuilder.InsertData(
+                table: "Dependents",
+                columns: new[] { "Id", "FirstName", "LastName", "Relationship", "DateOfBirth", "EmployeeId" },
+                values: new object[,]
+                {
+                    { 1, "Spouse", "Morant", 2, new DateTime(1998, 3, 3), 2 },
+                    { 2, "Child1", "Morant", 4, new DateTime(2020, 6, 23), 2 },
+                    { 3, "Child2", "Morant", 4, new DateTime(2021, 5, 18), 2 },
+                    { 4, "DP", "Jordan", 3, new DateTime(1974, 1, 2), 3 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
